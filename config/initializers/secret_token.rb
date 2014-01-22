18f905c034f6be5c3c9698fc00f81b36f6bef863
anna garcia <akaterinag88@gmail.com>
2014-01-22 13:52:23 -0430

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Curso::Application.config.secret_key_base = '3d14d1d611a13fa4fbbb1c6e51d27fcde021700d092475250b1b489369fdb004d1a5f91f9aab18bf3a4e1aa5f333cdc586540f67e219e2e0a92ba380afe0ce6b'
